2e131ccb99943c7b4cc95c8ea17a39350b728022
alex <alex@pdp7.net>
2020-04-03 21:42:18 +0200

    Draft a terrible first version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,60 @@
+use serde::{Deserialize, Serialize};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Tries to make it easy to run commands from Docker images.")]
+enum CmdockerArgs {
+    Wrapper {
+        wrapper: String,
+        args: Vec<String>,
+    },
+    AddWrapper {
+        name: String,
+        path: String,
+        image: String,
+    },
+}
+
+#[derive(Serialize, Deserialize)]
+struct Command {
+    path: String,
+    image: String,
+}
+
+#[derive(Serialize, Deserialize, Default)]
+struct CmdockerConfig {
+    commands: std::collections::HashMap<String, Command>,
+}
+
 fn main() {
+    let config: CmdockerConfig = confy::load("cmdocker").unwrap();
+    let arg0_str = std::env::args().next().unwrap();
+    let arg0 = std::path::Path::new(&arg0_str);
+    let not_wrapper = arg0.ends_with("cmdocker");
+    if !not_wrapper {
+        std::process::exit(run_wrapper(
+            config,
+            arg0_str,
+            std::env::args().skip(1).collect(),
+        ));
+    }
+    std::process::exit(match CmdockerArgs::from_args() {
+        CmdockerArgs::AddWrapper { name, path, image } => {
+            let mut config = config;
+            config.commands.insert(name, Command { path, image });
+            confy::store("cmdocker", config).unwrap();
+            0
+        }
+        CmdockerArgs::Wrapper { wrapper, args } => run_wrapper(config, wrapper, args),
+    });
+}
+
+fn run_wrapper(config: CmdockerConfig, wrapper: String, args: std::vec::Vec<String>) -> i32 {
     let user_dirs = directories::UserDirs::new().unwrap();
     let home = user_dirs.home_dir().to_str().unwrap();
-    let image = "busybox";
-    let command = "touch";
+    let command = config.commands.get(&wrapper).unwrap();
+    let image = &command.image;
+    let path = &command.path;
     std::process::Command::new("docker")
         .arg("run")
         .arg("-it")
@@ -20,8 +72,10 @@ fn main() {
             users::get_current_gid()
         ))
         .arg(image)
-        .arg(command)
-        .args(std::env::args().skip(1))
+        .arg(path)
+        .args(args)
         .status()
-        .unwrap();
+        .unwrap()
+        .code()
+        .unwrap()
 }
